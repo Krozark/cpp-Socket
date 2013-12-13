b56f0e9b1b8b27ce2f58b7e23e1f8e1d129c9ae6
Krozark <maxime.barbier1991@gmail.com>
2013-12-13 11:25:01 +0100

    add doc

--- include/Socket/SocketSerialized.hpp
+++ include/Socket/SocketSerialized.hpp
@@ -6,10 +6,24 @@
 
 namespace ntw {
 
+/**
+ * \brief A class tha merge Socket and Serializer.
+ * Use the << and >> operators to add or extract data in the socket.
+ */
 class SocketSerialized : public Serializer, public Socket
 {
     public:
+        /**
+         * \brief Constructor
+         * \param dommaine socket dommaine
+         * \param type socket type
+         * \param protocole socket protocol
+         */
         SocketSerialized(Socket::Dommaine dommaine,Socket::Type type,int protocole=0);
+
+        /**
+         * \brief Move constructor
+         */
         explicit SocketSerialized(Socket&& s);
         //~SocketSerialized();
 
@@ -18,12 +32,25 @@ class SocketSerialized : public Serializer, public Socket
         //inline Socket Wait(std::string host="",int port=PORT){return Socket::Wait(host,port);};
         //inline void Shutdown(Socket::Down mode=Socket::Down::BOTH){Socket::Shutdown(mode);};
 
+        /**
+         * \brief Send data in the buffer
+         */
         void send();
+
+        /**
+         * \brief store data into the internal buffer
+         */
         int receive();
 
         /// SERIALIZE
+        /**
+         * \brief clear the internal buffer.
+         */
         void clear();
 
+        /**
+         * \brief Short cut function tha call send() and clear()
+         */
         void inline sendCl(){send();clear();};
 
         SocketSerialized(const SocketSerialized&) = delete;
