843da11c99373bfa1659de7ad767bbe09e37069e
Krozark <maxime.barbier1991@gmail.com>
2014-05-12 00:10:51 +0200

    add doc in Status

--- include/Socket/Status.hpp
+++ include/Socket/Status.hpp
@@ -11,17 +11,8 @@ namespace ntw
         public:
             Status(short int c);///< constructor
 
-            /*friend Serializer& operator<<(Serializer& stream,const Status& self)
-              {
-              stream<<self.code;
-              return stream;
-              }
+            //friend Serializer& operator<<(Serializer& stream,const Status& self);
 
-              friend Serializer& operator>>(Serializer& stream,const Status& self)
-              {
-              stream>>self.code;
-              return stream;
-              }*/
             short int code; ///< code
 
             /**
@@ -29,16 +20,16 @@ namespace ntw
              * You can use yours as long as there a greater than user_define
              */
             enum : short int {
-                stop = -3,
-                wrong_id = -2,
-                ok = 0,
+                stop = -3, ///< set if the connexion is lost
+                wrong_id = -2, ///< set if a wrong function id is used with Client::call
+                ok = 0, //< all is good
                 request_add = 1,
                 broadcast_add = 2,
                 timeout = 3,
                 select = 4,
-                connexion = 5,
+                connexion = 5,///< set with sock.connect()
                 unknow = 404,
-                user_define
+                user_define ///< the maximal value use by the module. You can define yours after this one
             };
     };
 }
